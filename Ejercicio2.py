8b4f314c0a333823c80ff6858b105cf5f7bdc521
carlotamartin <91574355+carlotamartin@users.noreply.github.com>
2021-11-03 07:34:51 +0100

    cambio

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -6,9 +6,9 @@ def compareTriplets(a, b):
     for i in a:
         for j in b:
             if i>j:
-                resultado[0]+=1
+                resultado[0]==1
             elif i<j:
-                resultado[1]+=1
+                resultado[1]==1
     return resultado
 #Probamos el método
 a = (45, 50,99)
