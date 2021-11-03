991bdbaf14c838d42a9116a9a53fe409738ef63a
carlotamartin <91574355+carlotamartin@users.noreply.github.com>
2021-11-03 07:43:20 +0100

    errores

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -2,13 +2,17 @@
 # a y b son dos arrays que tienen cada uno tres elementos en el interior
 def compareTriplets(a, b):
     resultado = []
+    resultado_a = 0
+    resultado_b = 0
     #Inicializamos un bucle que recorra las dos listas y las podamos comparar
     for i in a:
         for j in b:
             if i>j:
-                resultado.insert(0, 1)
+                resultado_a +=1
+                resultado.insert(0, resultado_a )
             elif i<j:
-                resultado.insert(1, 1)
+                resultado_b += 1
+                resultado.insert(1, resultado_b)
     return resultado
 #Probamos el método
 a = (45, 50,99)
