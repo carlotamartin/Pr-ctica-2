4ce1801807a0461e9bc13d748b037df57f0f4f06
carlotamartin <91574355+carlotamartin@users.noreply.github.com>
2021-11-03 07:21:04 +0100

    inicio

---
+++ Ejercicio2.py
@@ -0,0 +1,4 @@
+# a y b son dos arrays que tienen cada uno tres elementos en el interior
+def compareTriplets(a, b):
+    #Inicializamos un bucle que recorra las dos listas y las recorra
+    if a[0]>
