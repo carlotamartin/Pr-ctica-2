3ae574f7a34c583ca807c2601ccf23879e8dc72c
carlotamartin <91574355+carlotamartin@users.noreply.github.com>
2021-11-03 07:33:31 +0100

    primera prueba del método

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -1,4 +1,16 @@
+#EJERCICIO: COMPARA LOS PROBLEMAS
 # a y b son dos arrays que tienen cada uno tres elementos en el interior
 def compareTriplets(a, b):
-    #Inicializamos un bucle que recorra las dos listas y las recorra
-    if a[0]>
+    resultado = []
+    #Inicializamos un bucle que recorra las dos listas y las podamos comparar
+    for i in a:
+        for j in b:
+            if i>j:
+                resultado[0]+=1
+            elif i<j:
+                resultado[1]+=1
+    return resultado
+#Probamos el método
+a = (45, 50,99)
+b= (95,50,99)
+print (compareTriplets(a,b))
