3e9da6534cad03af38b445f6eb9d3a713488ac52
carlotamartin <91574355+carlotamartin@users.noreply.github.com>
2021-11-03 11:16:41 +0100

    otra prueba

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -14,16 +14,19 @@ import numpy as np
 #
 def simpleArraySum(ar):
     # Write your code here
-    if __name__ == '__main__':
-        fptr = open(os.environ['OUTPUT_PATH'], 'w')
-        ar_count = int(input().strip())
-        ar = list(map(int, input().rstrip().split()))
-        result = simpleArraySum(ar)
-        fptr.write(str(result) + '\n')
-        fptr.close()
     return np.sum(ar)
 
 #Probamos la funcion
 b = np.array([[ 1,  2],[ 3,  4],[ 5,  6],[ 7,  8],[ 9, 10],[11, 12]])
 
 print(simpleArraySum(b))
+
+'''
+if __name__ == '__main__':
+        fptr = open(os.environ['OUTPUT_PATH'], 'w')
+        ar_count = int(input().strip())
+        ar = list(map(int, input().rstrip().split()))
+        result = simpleArraySum(ar)
+        fptr.write(str(result) + '\n')
+        fptr.close()
+'''
--- Ejercicio2.py
+++ Ejercicio2.py
@@ -1,7 +1,6 @@
 #EJERCICIO: COMPARA LOS PROBLEMAS
 # a y b son dos arrays que tienen cada uno tres elementos en el interior
 def compareTriplets(a, b):
-    resultado = []
     resultado_a = 0
     resultado_b = 0
     #Inicializamos un bucle que recorra las dos listas y las podamos comparar
@@ -9,11 +8,13 @@ def compareTriplets(a, b):
         for j in b:
             if i>j:
                 resultado_a +=1
-                resultado.insert(0, resultado_a )
+                #resultado.insert(0, resultado_a )
             elif i<j:
                 resultado_b += 1
-                resultado.insert(1, resultado_b)
+                #resultado.insert(1, resultado_b)
+    resultado = [resultado_a, resultado_b]
     return resultado
+
 #Probamos el método
 a = (45, 50,99)
 b= (95,50,99)
