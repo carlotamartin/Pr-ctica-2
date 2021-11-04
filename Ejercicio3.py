2df5cc1a25948246a7ae53c903ebb4db1d1b7506
carlotamartin <91574355+carlotamartin@users.noreply.github.com>
2021-11-04 12:26:18 +0100

    ejercicio 3

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -1,3 +1,16 @@
 #Importamos librerías
 import numpy as np
-#import da as da
+'''
+El ejercicio es el mismo que el ejercicio1.py pero en
+este caso si python ve que es una variable de tipo
+long en vez de una variable de tipo integer
+'''
+def aVeryBigSum(ar):
+    suma = np.sum(ar)
+    # Write your code here
+    return print("La suma de todos los elementos del array es: "+ str(suma))
+#Probamos la funcion
+b = np.array([[ 1,  2],[ 3,  4],[ 5,  6],[ 7,  8],[ 9, 10],[11, 12]])
+
+aVeryBigSum(b)
+
