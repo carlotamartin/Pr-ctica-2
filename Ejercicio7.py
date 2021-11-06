bc63e8b229bcc535142044e1bf96c8f80acde507
carlotamartin <91574355+carlotamartin@users.noreply.github.com>
2021-11-06 09:07:38 +0100

    errores

--- Ejercicio7.py
+++ Ejercicio7.py
@@ -6,15 +6,17 @@ def gradingStudents(grades):
             return print ('Estudiante '+ str(num_estudiante) + ' recibió '+ str(i)+ ' una califiación por debajo de 40, por lo que la calificación no se modificará y la calificación final del estudiante es '+ str(i))
         if i>=40:
             multiplo = i % 5
-            if multiplo<3:
-                num_final = i+ multiplo
-                return print ('Estudiante '+ str(num_estudiante) + ' recibió un '+ str(i)+ ' y el siguiente múltiplo de 5 de ' +str(i) +' es '+ str(num_final)+ '. Ya que '+ str(num_final)+'-'+str(i) +'<3, la calificación del estudiante se redondeará a '+str(num_final))
-            elif multiplo==3:
-                num_final = i
-                return print ('Estudiante '+ str(num_estudiante) + ' recibió un '+ str(i)+ ' y el siguiente múltiplo de 5 de ' +str(i) +' es '+ str(num_final)+ '. Ya que '+ str(num_final)+'-'+str(i) +'=3, la calificación del estudiante no se modificará y la calificación final del estudiante es: '+str(num_final))
+            num_mult = i+ multiplo
+            resta = num_mult-multiplo
+            if resta<3:
+                #num_final = i+ multiplo
+                return print ('Estudiante '+ str(num_estudiante) + ' recibió un '+ str(i)+ ' y el siguiente múltiplo de 5 de ' +str(i) +' es '+ str(num_mult)+ '. Ya que '+ str(num_mult)+'-'+str(i) +'<3, la calificación del estudiante se redondeará a '+str(num_mult))
+            elif resta==3:
+                #num_final = i
+                return print ('Estudiante '+ str(num_estudiante) + ' recibió un '+ str(i)+ ' y el siguiente múltiplo de 5 de ' +str(i) +' es '+ str(num_mult)+ '. Ya que '+ str(num_mult)+'-'+str(i) +'=3, la calificación del estudiante no se modificará y la calificación final del estudiante es: '+str(i))
             else:
-                num_final = i
-                return print ('Estudiante '+ str(num_estudiante) + ' recibió un '+ str(i)+ ' y el siguiente múltiplo de 5 de ' +str(i) +' es '+ str(num_final)+ '. Ya que '+ str(num_final)+'-'+str(i) +'>3, la calificación del estudiante no se modificará y la calificación final del estudiante es: '+str(num_final))
+                #num_final = i
+                return print ('Estudiante '+ str(num_estudiante) + ' recibió un '+ str(i)+ ' y el siguiente múltiplo de 5 de ' +str(i) +' es '+ str(num_mult)+ '. Ya que '+ str(num_mult)+'-'+str(i) +'>3, la calificación del estudiante no se modificará y la calificación final del estudiante es: '+str(i))
 
 #Probamos el método
 grades = [73,67,38,33]
