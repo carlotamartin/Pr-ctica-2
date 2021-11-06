c6a5f2da087a0a490e0d66d4fab39fbcc6725339
carlotamartin <91574355+carlotamartin@users.noreply.github.com>
2021-11-06 11:47:38 +0100

    arreglo de iguales

--- Ejercicio8.py
+++ Ejercicio8.py
@@ -32,13 +32,13 @@ def countApplesAndOranges(s, t, a, b, apples, oranges):
         resultado_manzanas += posicion_fruta(s, t, a, i)
     for i in oranges :
         resultado_naranjas += posicion_fruta(s, t, b, i)
-    print('La cantidad de manzanas que caen en la casa de Sam son: '+resultado_manzanas)
-    print('La cantidad de naranjas que caen en la casa de Sam son: '+resultado_naranjas)
+    print('La cantidad de manzanas que caen en la casa de Sam son: '+str(resultado_manzanas))
+    print('La cantidad de naranjas que caen en la casa de Sam son: '+str(resultado_naranjas))
 
 def posicion_fruta (s, t, pos_arbol, pos_fruta):
     resultado = 0
     posicion= pos_arbol+pos_fruta
-    if posicion>s and posicion<t:
+    if posicion>=s and posicion<=t:
         resultado +=1
     else:
         resultado= 0
@@ -52,5 +52,5 @@ pos_manzano = 5
 pos_naranjo = 15
 manzanas = [-2, 2, 1]
 naranjas= [5,-6]
-
+countApplesAndOranges(punto_partida,punto_final,pos_manzano, pos_naranjo, manzanas, naranjas)
 
